d7fa720fd7c9192e15d78dd885fe3a7d845b5720
S4mur419020 <murai.tamas@diak.szamalk-szalezi.hu>
2024-10-17 09:25:08 +0200

    2. és 3. feladata

--- feladatok.py
+++ feladatok.py
@@ -15,3 +15,27 @@ def fel1(lista):
         i+=1
         
     return db
+
+def fel2(lista):
+    i:int=0
+    ossz:int=0
+    while(i<len(lista)):
+        print(lista[i])
+        if(lista[i]<0):
+            ossz+=lista[i]
+        i+=1
+        
+    return ossz
+
+def fel3(lista):
+    i:int=0
+    db:int=0
+    osszeg:int=0
+    while(i<len(lista)):
+        if(lista[i]%5==0):
+            osszeg+=lista[i]
+            db+=1
+        i+=1
+    atlag:float=osszeg/db
+    return atlag
+
--- main.py
+++ main.py
@@ -4,3 +4,11 @@ lista=[12, 21,56,32,-5,-23,35]
 print("Első feladat.")
 db:int=feladatok.fel1(lista)
 print(f"A pozitív számok száma: {db}")
+
+print("Második feladat.")
+ossz:int=feladatok.fel2(lista)
+print(f"A negatív számok összege: {ossz}")
+
+print("Harmadik feladat.")
+atlag:float=feladatok.fel3(lista)
+print(f"Az 5-tel osztható számok átlaga: {atlag}")
