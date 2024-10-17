94990b9f179503a6271af33cceae1c37e83bdd5a
S4mur419020 <murai.tamas@diak.szamalk-szalezi.hu>
2024-10-17 11:17:54 +0200

    A cinkelt kocka nélkül van meg a többi

--- feladatok.py
+++ feladatok.py
@@ -1,3 +1,4 @@
+import random 
 '''lista
 lista[12, 21,56,32,-5,-23,35]
 Az alábbi metodusok  paraméterként kapják a fenti listát
@@ -38,4 +39,74 @@ def fel3(lista):
         i+=1
     atlag:float=osszeg/db
     return atlag
+
+def fel4():
+    fejiras_lista=[]
+    i:int=0
+    while(i<10):
+        erme:int=int(random.random()*2)+1
+        if(erme==2):
+            fejiras_lista.append("Írás")
+        elif(erme==1):
+            fejiras_lista.append("Fej")
+        i+=1
+    return fejiras_lista
+
+def fel4_2(fejiras_lista):
+    i:int=0
+    fej:int=0
+    while(i<len(fejiras_lista)):
+        if(fejiras_lista[i]=="Fej"):
+            fej+=1
+        i+=1
+        
+    return fej
+
+
+def fel5():
+    dobas_lista=[]
+    i:int=0
+    while(i<200):
+        dobas:int=int(random.random()*7)+1
+        if(dobas==1):
+            dobas_lista.append(1)
+        elif(dobas==2):
+            dobas_lista.append(2)
+        elif(dobas==3):
+            dobas_lista.append(3)
+        elif(dobas==4):
+            dobas_lista.append(4)
+        elif(dobas==5):
+            dobas_lista.append(5)
+        else:
+            dobas_lista.append(6)
+        i+=1
+
+    return dobas_lista
+
+
+def fel5_2(dobas_lista):
+    i:int=0
+    egy:int=0
+    ketto:int=0
+    harom:int=0
+    negy:int=0
+    ot:int=0
+    hat:int=0
+    while(i<len(dobas_lista)):
+        if(dobas_lista[i]==1):
+            egy+=1
+        elif(dobas_lista[i]==2):
+            ketto+=1
+        elif(dobas_lista[i]==3):
+            harom+=1
+        elif(dobas_lista[i]==4):
+            negy+=1
+        elif(dobas_lista[i]==5):
+            ot+=1
+        elif(dobas_lista[i]==6):
+            hat+=1
+        i+=1
+        
+    return egy, ketto, harom, negy, ot, hat
 
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import feladatok
 lista=[12, 21,56,32,-5,-23,35]
 
-print("Első feladat.")
+
+
+'''print("Első feladat.")
 db:int=feladatok.fel1(lista)
 print(f"A pozitív számok száma: {db}")
 
@@ -12,3 +14,20 @@ print(f"A negatív számok összege: {ossz}")
 print("Harmadik feladat.")
 atlag:float=feladatok.fel3(lista)
 print(f"Az 5-tel osztható számok átlaga: {atlag}")
+
+print("Negyedik feladat.")
+fejiras_lista=feladatok.fel4()
+print(fejiras_lista)
+
+print("Negyedik feladat második része.")
+fej:int=feladatok.fel4_2(fejiras_lista)
+print(f"Ennyiszer volt fej: {fej}")'''
+
+print("Ötödik feladat.")
+dobas_lista:int=feladatok.fel5()
+print(dobas_lista)
+
+print("Ötödik feladat második része.")
+egy, ketto, harom, negy, ot, hat=feladatok.fel5_2(dobas_lista)
+print(f"Eggyesből: {egy}, Kettesből: {ketto}, Hármasból: {harom}, Negyesből: {negy}, Ötösből: {ot}, Hatosból: {hat} darab volt.")
+
